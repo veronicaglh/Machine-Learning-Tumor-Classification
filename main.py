219682581ba89f26bfb0aa09c16d9b636a794823
veronicaglh <veronicagt.IT@gmail.com>
2023-03-23 20:27:48 +0300

    add program description

---
+++ main.py
@@ -0,0 +1,15 @@
+# This project will use machine learning to classify a tumor as Benign(non-cancerous) or Malignant(Cancerous)
+# The dataset used for this program can be accessed here: https://www.kaggle.com/datasets/uciml/breast-cancer-wisconsin-data
+# Users can use the input given by the dataset and submit it into the entry field of the GUI. 
+
+# The code for this project has been classified into two parts 
+# The first part will be dealing with building the GUI(graphical user interface) - using tkinter 
+# The second part will deal with the implementing the computational aspect. 
+# This program uses various python libraries to work 
+# Inorder to access the libraries they must first be installed using the following commands: 
+# pip install pillow 
+# pip install numpy 
+# pip install pandas
+# pip install -U scikit-learn
+# The program needs these libraries in order to work. 
+
